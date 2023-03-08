e2f035af3a23dbfd4cb4a61a683bc77d314576d9
Ltwoz <warrapat.102@gmail.com>
2023-03-08 13:59:24 +0700

    messenger chat

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ToastContextProvider } from "@/components/contexts/toast-context";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 export default function App({ Component, pageProps }) {
     const { session } = pageProps;
@@ -9,6 +10,10 @@ export default function App({ Component, pageProps }) {
         <SessionProvider session={session}>
             <ToastContextProvider>
                 <Component {...pageProps} />
+                <MessengerCustomerChat
+                    pageId="219615218531098"
+                    appId="935203817615198"
+                />
             </ToastContextProvider>
         </SessionProvider>
     );
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 export default function Document() {
     return (
@@ -7,10 +6,6 @@ export default function Document() {
             <Head />
             <body>
                 <Main />
-                <MessengerCustomerChat
-                    pageId="219615218531098"
-                    appId="935203817615198"
-                />
                 <div id="portal" />
                 <NextScript />
             </body>
