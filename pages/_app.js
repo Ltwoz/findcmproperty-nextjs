90d9c695bf427a8272a9f74108e70120f943396c
Ltwoz <warrapat.102@gmail.com>
2023-03-10 13:39:39 +0700

    GATG

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,51 @@
 import { ToastContextProvider } from "@/components/contexts/toast-context";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import * as gtag from "@/lib/gtag";
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
     const { session } = pageProps;
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
 
     return (
-        <SessionProvider session={session}>
-            <ToastContextProvider>
-                <Component {...pageProps} />
-            </ToastContextProvider>
-        </SessionProvider>
+        <>
+            <Head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${gtag.GA_TRACKING_ID}', {
+                                page_path: window.location.pathname,
+                            });
+                        `,
+                    }}
+                />
+            </Head>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <SessionProvider session={session}>
+                <ToastContextProvider>
+                    <Component {...pageProps} />
+                </ToastContextProvider>
+            </SessionProvider>
+        </>
     );
 }
