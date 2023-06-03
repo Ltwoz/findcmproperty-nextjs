ea887dd218421ceaaf678d662765d07c5f4d45a7
Ltwoz <warrapat.102@gmail.com>
2023-06-03 16:28:23 +0700

    change key env name

--- utils/s3.js
+++ utils/s3.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const uploadFile = async (files) => {
     console.log("files to upload s3 :", files);
